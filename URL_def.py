44cd1528b9d09cfd23e4c0748e78c03a6b62a114
Anton Galkin <artemis9898@mail.ru>
2017-12-20 17:29:50 +0300

    Update URL_def.py

--- URL_def.py
+++ URL_def.py
@@ -1,16 +1,20 @@
-from urllib.request import urlopen
+# Галкин Антон 
+# Задание - На основе кода с выполнением предыдущего задания (см. тему 18) 
+# реализуйте получение JSON с удаленного хоста и красивый вывод информации на экран (через функцию). 
+
+from urllib.request import urlopen 
 import pprint
 import json
 
 
-def hacker_man(id):
+def hacker_man(id):   # на вход подаём цифру-id пользователя на выходе имеем некоторые основные данные (возможен вывод большего числа данных)
     inquiry = "https://api.vk.com/method/users.get?user_ids={id}&fields=education&v=5.69".format(id = id)
 
     try:
         inquiry_obj = urlopen(inquiry)
         obj = json.loads(inquiry_obj.read())
     except:
-        print('Ошибка!')
+        print('Ошибка')
     return obj
 
 
